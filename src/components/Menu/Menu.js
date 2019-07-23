d80799ac527f8447b16936e9abbdca4a7e206181
omar <omasrt@gmail.com>
2019-07-23 15:36:50 -0500

    first commit

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Menu() {
+  return (
+    <div>
+      <p>Menu</p>
+    </div>
+  );
+}
+
+export default Menu;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <div>
+      <p>Navigation</p>
+    </div>
+  );
+}
+
+export default Navigation;
