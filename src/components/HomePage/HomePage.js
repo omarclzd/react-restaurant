fdc79f4c7336323e6868e5f00b5f238c37fce2f7
omar <omasrt@gmail.com>
2019-07-23 17:14:01 -0500

    sets animation to home h1

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,26 @@
 import React from "react";
 
+import { MDBContainer, MDBRow, MDBCol, MDBAnimation } from "mdbreact";
+
 function HomePage() {
   return (
-    <div>
-      <p>HomePage</p>
-    </div>
+    <MDBContainer>
+      <MDBRow className="text-center">
+        <MDBCol>
+          <MDBAnimation type="bounce">
+            <h1>
+              <strong>Omar's Restaurant</strong>
+            </h1>
+          </MDBAnimation>
+          <h2>Mexican Cuizine</h2>
+
+          <h3>AUSTIN, TEXAS</h3>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow className="text-center">
+        <MDBCol />
+      </MDBRow>
+    </MDBContainer>
   );
 }
 
