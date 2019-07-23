7b1e0f4b7f8e7f38ec5ec131471647acc88f57d8
omar <omasrt@gmail.com>
2019-07-23 16:36:48 -0500

    sets navbar with mdbreact

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse
+} from "mdbreact";
 import * as ROUTES from "../../constant/routes";
 
-function Navigation() {
-  return (
-    <div>
-      <Link to={ROUTES.HOME_PAGE}>Home</Link>
-      <Link to={ROUTES.MENU_PAGE}>Menu</Link>
-      <Link to={ROUTES.ABOUT_PAGE}>About</Link>
-      <Link to={ROUTES.GALLERY_PAGE}>Gallery</Link>
-    </div>
-  );
+class Navigation extends Component {
+  state = {
+    isOpen: false
+  };
+
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
+  render() {
+    return (
+      <MDBNavbar color="indigo" dark expand="md">
+        <MDBNavbarBrand>
+          <strong className="white-text">Navbar</strong>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler onClick={this.toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+          <MDBNavbarNav left>
+            <MDBNavItem active>
+              <MDBNavLink to={ROUTES.HOME_PAGE}>Home</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to={ROUTES.MENU_PAGE}>Menu</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to={ROUTES.ABOUT_PAGE}>About</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to={ROUTES.GALLERY_PAGE}>Gallery</MDBNavLink>
+            </MDBNavItem>
+          </MDBNavbarNav>
+          <MDBNavbarNav right />
+        </MDBCollapse>
+      </MDBNavbar>
+    );
+  }
 }
 
 export default Navigation;
